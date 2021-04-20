8523414ad392633f53b23be04daef5b03deb130d
Savitar97 <atoth1571@gmail.com>
2021-04-20 05:42:17 +0200

    Closes #4; Override the prompt provider getPrompt method to replace the default prompt.

---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/promptconfig/CustomPromptProvider.java
@@ -0,0 +1,14 @@
+package com.epam.training.ticketservice.promptconfig;
+
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomPromptProvider implements PromptProvider {
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString("Ticket service>",AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN));
+    }
+}
